9e9299f57b058cb14e22aaa2bb8d2448c7cb3fb4
Abhishek Kumar <abhishekkgf999@gmail.com>
2025-12-19 23:23:32 +0530

    made CSS JS CLOCK

---
+++ PROJECTS/02_CSS_JS_CLOCK/script.js
@@ -0,0 +1,20 @@
+const secondHand = document.querySelector('.second-hand');
+const hourHand = document.querySelector('.hour-hand');
+const minHand = document.querySelector('.min-hand');
+
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds/60)*360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegree = ((hours/12)*360)+90;
+    hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegree = ((minutes/60)*360)+90;
+    minHand.style.transform = `rotate${minutesDegree}deg`;
+}
+
+setInterval(setDate, 1000);
